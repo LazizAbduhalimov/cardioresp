8ef1d9d64ae1f5cf361c4a71b4bcc5f80db7b570
LazizGameDeveloper <78848741+LazizGameDeveloper@users.noreply.github.com>
2023-03-03 18:27:04 +0500

    Cache was removed
    #

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,9 +5,9 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('tag-cloud/', TagCloudPage.as_view(), name="tag-cloud"),
-    path('issue/', cache_page(60)(ArchiveView.as_view()), name="issue"),
-    path('issue/<slug:slug>/', cache_page(60)(IssueDetail.as_view()), name="issue-detail"),
-    path('issue/article/<slug:slug>/', cache_page(60*2)(ArticleView.as_view()), name="article"),
+    path('issue/', ArchiveView.as_view(), name="issue"),
+    path('issue/<slug:slug>/', IssueDetail.as_view(), name="issue-detail"),
+    path('issue/article/<slug:slug>/', ArticleView.as_view(), name="article"),
     path("pdf/article/<slug:slug>.pdf", PdfView.as_view(), name="pdf-view"),
     path("pdf/volume/<slug:slug>", PdfViewVolume.as_view(), name="pdf-view-volume"),
 ]
--- main_app/urls.py
+++ main_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("favicon.ico", favicon),
     path('authors/', AuthorsPage.as_view(), name="authors"),
     path('authors/<slug:slug>/', AuthorsDetailPage.as_view(), name="author-detail"),
-    path('submission/', cache_page(60*15)(SubmissionPage.as_view()), name="submission"),
+    path('submission/', SubmissionPage.as_view(), name="submission"),
     path('editorial-office/', EditorialPage.as_view(), name="editorial-team"),
     path('editorial-office/<slug:slug>', EditorialMemberPage.as_view(), name="editorial-member"),
-    path('<slug:slug>/', cache_page(60*15)(SamePages.as_view()), name="same-pages"),
+    path('<slug:slug>/', SamePages.as_view(), name="same-pages"),
 ]
